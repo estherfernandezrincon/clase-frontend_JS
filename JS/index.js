54cb2d6dd71be339dc1fdc360bccd40274b3e359
Esther <efr78@hotmail.com>
2021-09-20 23:55:11 +0200

    prevent default

--- JS/index.js
+++ JS/index.js
@@ -1,48 +1,60 @@
-const tweets = [
-    {
-        author: "@alberto",
-        message: "Lorem fistrum a peich benemeritaar amatomaa mamaar de la",
-        date: "2021-02-09 20:54:23"
-    },
-    {
-        author: "@chiquito",
-        message: "Está la cosa muy malar torpedo llevame al sircoo la caidita benemeritaar",
-        date: "2021-02-08 23:45:21"
-    },
-    {
-        author: "@chiquito",
-        message: "Está la cosa muy malar torpedo llevame al sircoo la caidita benemeritaar",
-        date: "2021-02-08 23:45:21"
+window.addEventListener('DOMContentLoaded', function () {
+    const tweets = [
+        {
+            author: "@alberto",
+            message: "Lorem fistrum a peich benemeritaar amatomaa mamaar de la",
+            date: "2021-02-09 20:54:23"
+        },
+        {
+            author: "@chiquito",
+            message: "Está la cosa muy malar torpedo llevame al sircoo la caidita benemeritaar",
+            date: "2021-02-08 23:45:21"
+        },
+        {
+            author: "@chiquito",
+            message: "Está la cosa muy malar torpedo llevame al sircoo la caidita benemeritaar",
+            date: "2021-02-08 23:45:21"
+        }
+    ];
+
+    function loadTweets() {
+
+        const list = document.querySelector('.post-list')
+
+        for (const tweet of tweets) {
+            const tweetElement = document.createElement('article') //creamos el padre
+            const tweetHTML = `<div class="post">
+                <strong class="author">${tweet.author}</strong>
+                <p class="message">${tweet.message}</p>
+                <time datetime="${tweet.date}">${tweet.date}</time>
+            </div>`
+
+            tweetElement.innerHTML = tweetHTML; // inyectamos los textos
+            list.appendChild(tweetElement) //le decimos donde meterlos
+        }
+
+        //seleccionamos el nodo del loader
+
+        const loader = document.querySelector('.lds-ring')
+        loader.classList.toggle('hidden')
     }
-];
-
-function loadTweets() {
-
-    const list = document.querySelector('.post-list')
-
-    for (const tweet of tweets) {
-        const tweetElement = document.createElement('article') //creamos el padre
-        const tweetHTML = `<div class="post">
-            <strong class="author">${tweet.author}</strong>
-            <p class="message">${tweet.message}</p>
-            <time datetime="${tweet.date}">${tweet.date}</time>
-        </div>`
-
-        tweetElement.innerHTML = tweetHTML; // inyectamos los textos
-        list.appendChild(tweetElement) //le decimos donde meterlos
-    }
-
-    //seleccionamos el nodo del loader
-
-    const loader = document.querySelector('.lds-ring')
-    loader.classList.toggle('hidden')
-}
 
 
 
-//seleccionamosel boton
+    //seleccionamos el boton
 
-const button = document.querySelector('button')
+    const button = document.querySelector('button')
 
-button.addEventListener('click', loadTweets) // los tweets se cargan al cliclar el boton.
-//no se pone loadTweets() para qeu no la ejecute al cargar la web
+    button.addEventListener('click', loadTweets) // los tweets se cargan al cliclar el boton.
+    //no se pone loadTweets() para qeu no la ejecute al cargar la web
+
+
+    //evitamos la accion por defecto del link
+
+    document.querySelector('a').addEventListener('click', function (ev) {
+        ev.preventDefault()
+        //ahora le pasamos el link a otra web 
+        window.location.href = 'https://developer.mozilla.org'
+    })
+
+})
